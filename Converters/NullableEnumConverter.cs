1c333dee843ed688f1cc490e7743b2707057f8e0
Chetan Pandey <chetanpandey29@gmail.com>
2019-07-01 18:08:43 +0530

    Code
    Committed

---
+++ Converters/NullableEnumConverter.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using Newtonsoft.Json;
+using ShopifySharp.Enums;
+using System.Runtime.Serialization;
+
+namespace ShopifySharp.Converters
+{
+    /// <summary>
+    /// A custom enum converter for all enums which returns the value 
+    /// as null when the value is null or does not exist.
+    /// </summary>
+    public class NullableEnumConverter<T> : StringEnumConverter where T : struct
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            T parsed;
+            
+            if (!Enum.TryParse(reader.Value?.ToString() ?? "", true, out parsed))
+            {
+                // Some EnumMember values have an '_', '-' or '/' in their value and will fail the TryParse or IsDefined checks.
+                // Use reflection to pull all of the enums values, get their EnumMember value and check if there's a match.
+
+                var enumType = typeof(T);
+                var enumVals = Enum.GetValues(enumType);
+
+                foreach (var enumVal in enumVals)
+                {
+                    var valInfo = enumType.GetMember(enumVal.ToString());
+                    var enumMember = valInfo[0].GetCustomAttributes(typeof(EnumMemberAttribute), false);
+
+                    if (enumMember.Length == 0)
+                    {
+                        continue;
+                    }
+
+                    if (((EnumMemberAttribute)enumMember[0]).Value?.ToString() == reader.Value?.ToString())
+                    {
+                        return (T) enumVal;
+                    }
+                }
+
+                //No match found. Return null.
+                return null;
+            }
+
+            return parsed;
+        }
+    }
+}
---
+++ Extensions/EnumExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace ShopifySharp
+{
+    /// <summary>
+    /// Enum Extension Method
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Reads and uses the enum's <see cref="EnumMemberAttribute"/> for serialization. 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string ToSerializedString(this Enum input)
+        {
+            string name = input.ToString();
+            MemberInfo[] info = input.GetType().GetMember(name);
+
+            if (info.Length > 0)
+            {
+                var attribute = info[0].GetCustomAttribute<EnumMemberAttribute>();
+
+                if(attribute != null)
+                {
+                    return attribute.Value;
+                }
+            }
+
+            return name.ToLower();
+        }
+
+        /// <summary>
+        /// Convert list of Enums to a comma seperated string
+        /// </summary>
+        public static string EnumListToString<T>(IEnumerable<T> enumList)
+        {
+            var list = new List<string>();
+
+            if (enumList != null && enumList.Any())
+            {
+                foreach (var enumItem in enumList)
+                {
+                    list.Add(EnumExtensions.ToSerializedString(enumItem as Enum));
+                }
+            }
+            return string.Join(",", list);
+        }
+    }
+}
